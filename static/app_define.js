231869019e52808cf54d6339da778509296d6842
tamvh <tamvh@tams-MBP.lan>
2017-08-15 11:51:14 +0700

    update

--- static/app_define.js
+++ static/app_define.js
@@ -6,6 +6,6 @@ var ZALOPAY_URL = 'https://zalopay.com.vn/openapp/index.html';
 var UPLOAD_URL = "http://gbc.zing.vn/iot/upload/controller/";
 //var UPLOAD_URL = 'https://vpos.zing.vn/vpos/upload/controller/';
 var RESOURCE_URL = "http://gbc.zing.vn";
-var MERCHANT_CODE = "maybanhangtudong"; 
+var MERCHANT_CODE = "vpos"; 
 var HTTP_TRANSPRO = "https://gbcstaging.zing.vn/vpos/api/invoice";
 //var HTTP_TRANSPRO = "http://192.168.4.241:8081/vpos/api/invoice";
