0eb0e235443b4abf87fc89f456357dc08898c8ad
tamvh <tamvh@Huys-MacBook-Pro.local>
2017-12-15 00:48:26 +0700

    update

--- static/product/product.controller.js
+++ static/product/product.controller.js
@@ -17,7 +17,7 @@
         $scope.total_item_in_category = 0;
         var dt_items = [];
         var img_host = "";
-        
+
         function get_params() {
             var cur_url = window.location.href;
             if (cur_url.split('?').length === 2) {
@@ -90,14 +90,6 @@
             var _price = 0;
             var _total_amout = 0;
 
-//            var foods = $cookies.get("fooditems");
-//            console.log('food: ' + foods);
-//            var arr_food = [];
-//            if (foods + '' !== '' && foods + '' !== 'undefined') {
-//                arr_food = JSON.parse(foods);
-//                $rootScope.foodItems = arr_food;
-//            }
-
             for (var j in $rootScope.foodItems) {
                 _soluong = $rootScope.foodItems[j].quantity;
                 _price = $rootScope.foodItems[j].price;
@@ -107,6 +99,24 @@
             $rootScope.totalmoney = _total_amout;
             $cookies.put("totalmoney", $rootScope.totalmoney);
         };
+
+        function xoa_dau(str) {
+            str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+            str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+            str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+            str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+            str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+            str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+            str = str.replace(/đ/g, "d");
+            str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+            str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+            str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+            str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+            str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+            str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+            str = str.replace(/Đ/g, "D");
+            return str;
+        }
 
         $scope.getListProduct = function () {
             initJsBrige();
@@ -133,18 +143,11 @@
 
                             }
                             dt_items = response.dt.items;
-
-//                            var foods = $cookies.get("fooditems");
-//                            var arr_food = [];
-//                            if (foods + '' !== '' && foods + '' !== 'undefined') {
-//                                arr_food = JSON.parse(foods);
-//                                $rootScope.foodItems = arr_food;
-//                            }
                             if ($rootScope.foodItems.length > 0) {
                                 for (var i in dt_items) {
                                     if (dt_items[i].status === 1) {
                                         dt_items[i].name_view = dt_items[i].item_name;
-                                        
+                                        dt_items[i].name_view_noaccent = xoa_dau(dt_items[i].item_name);
                                         if (checkExistImgUrl(dt_items[i].img_path)) {
                                             dt_items[i].img_path = img_host + dt_items[i].img_path;
                                         } else {
@@ -181,7 +184,7 @@
                                     if (dt_items[i].status === 1) {
                                         dt_items[i].bgcolor = "white";
                                         dt_items[i].name_view = dt_items[i].item_name;
-                                        
+                                        dt_items[i].name_view_noaccent = xoa_dau(dt_items[i].item_name);
                                         dt_items[i].quantity = 0;
                                         if (checkExistImgUrl(dt_items[i].img_path)) {
                                             dt_items[i].img_path = img_host + dt_items[i].img_path;
